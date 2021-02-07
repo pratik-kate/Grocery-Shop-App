7be5c39e127513329727b434502d5589278c9b95
pratikkate3 <pratikrkate@gmail.com>
2021-02-07 12:29:12 +0530

    bottom navigation added

--- app/src/main/java/com/supertrident/onlinestore/MainActivity.java
+++ app/src/main/java/com/supertrident/onlinestore/MainActivity.java
@@ -1,14 +1,66 @@
 package com.supertrident.onlinestore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+
+import com.gauravk.bubblenavigation.BubbleNavigationConstraintView;
+import com.gauravk.bubblenavigation.BubbleNavigationLinearView;
+import com.gauravk.bubblenavigation.listener.BubbleNavigationChangeListener;
+import com.supertrident.onlinestore.fragmets.CartFragment;
+import com.supertrident.onlinestore.fragmets.HomeFragment;
+import com.supertrident.onlinestore.fragmets.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout screen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        screen = findViewById(R.id.screen);
+        HomeFragment fragment = new HomeFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.screen,fragment);
+        transaction.commit();
+
+        BubbleNavigationConstraintView bubbleNavigation;
+        bubbleNavigation  = findViewById(R.id.bottom_navigation);
+        bubbleNavigation.setNavigationChangeListener(new BubbleNavigationChangeListener() {
+            @Override
+            public void onNavigationChanged(View view, int position) {
+                //navigation changed, do something
+                switch (position){
+                    case 0: {
+                        HomeFragment fragment = new HomeFragment();
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.screen,fragment);
+                        transaction.commit();
+                        break;
+                    }
+                    case 1:{
+                        CartFragment fragment = new CartFragment();
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.screen,fragment);
+                        transaction.commit();
+                        break;
+                    }
+                    case 2:{
+                        ProfileFragment fragment = new ProfileFragment();
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.screen,fragment);
+                        transaction.commit();
+                        break;
+                    }
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/supertrident/onlinestore/fragmets/CartFragment.java
@@ -0,0 +1,30 @@
+package com.supertrident.onlinestore.fragmets;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.supertrident.onlinestore.R;
+
+
+public class CartFragment extends Fragment {
+
+
+    public CartFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_cart, container, false);
+
+        return view;
+    }
+}
