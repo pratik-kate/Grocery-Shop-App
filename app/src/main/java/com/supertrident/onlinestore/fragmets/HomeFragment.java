6a1d6a289f51072214a17e6c91affc6a654f98a1
pratikkate3 <pratikrkate@gmail.com>
2021-02-09 11:47:33 +0530

    Product banner slider added

--- app/src/main/java/com/supertrident/onlinestore/fragmets/HomeFragment.java
+++ app/src/main/java/com/supertrident/onlinestore/fragmets/HomeFragment.java
@@ -4,14 +4,29 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+import com.supertrident.onlinestore.MainActivity;
 import com.supertrident.onlinestore.R;
 
-public class HomeFragment extends Fragment {
+import java.util.HashMap;
 
+public class HomeFragment extends Fragment implements BaseSliderView.OnSliderClickListener,
+        ViewPagerEx.OnPageChangeListener {
+
+    SliderLayout sliderLayout ;
+    HashMap<String, String> HashMapForURL ;
+
+    HashMap<String, Integer> HashMapForLocalRes ;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -23,6 +38,100 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        sliderLayout = (SliderLayout)view.findViewById(R.id.slider);
+
+        //Call this method if you want to add images from URL .
+        //AddImagesUrlOnline();
+
+        //Call this method to add images from local drawable folder .
+        AddImageUrlFormLocalRes();
+
+        //Call this method to stop automatic sliding.
+        //sliderLayout.stopAutoCycle();
+
+        for(String name : HashMapForLocalRes.keySet()){
+
+            TextSliderView textSliderView = new TextSliderView(getContext());
+
+            textSliderView
+                    .description(name)
+                    .image(HashMapForLocalRes.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
+                        @Override
+                        public void onSliderClick(BaseSliderView slider) {
+                            Toast.makeText(getContext(), "clicked", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+            textSliderView.bundle(new Bundle());
+
+            textSliderView.getBundle()
+                    .putString("extra",name);
+
+            sliderLayout.addSlider(textSliderView);
+        }
+        sliderLayout.setPresetTransformer(SliderLayout.Transformer.DepthPage);
+
+        sliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+
+        sliderLayout.setCustomAnimation(new DescriptionAnimation());
+
+        sliderLayout.setDuration(3000);
+
+        sliderLayout.addOnPageChangeListener(this);
+
         return view;
+    }
+    @Override
+    public void onStop() {
+
+        sliderLayout.stopAutoCycle();
+
+        super.onStop();
+    }
+
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+    @Override
+    public void onPageSelected(int position) {
+
+        Log.d("Slider Demo", "Page Changed: " + position);
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    public void AddImagesUrlOnline(){
+
+        HashMapForURL = new HashMap<String, String>();
+
+        HashMapForURL.put("CupCake", "http://androidblog.esy.es/images/cupcake-1.png");
+        HashMapForURL.put("Donut", "http://androidblog.esy.es/images/donut-2.png");
+        HashMapForURL.put("Eclair", "http://androidblog.esy.es/images/eclair-3.png");
+        HashMapForURL.put("Froyo", "http://androidblog.esy.es/images/froyo-4.png");
+        HashMapForURL.put("GingerBread", "http://androidblog.esy.es/images/gingerbread-5.png");
+    }
+
+    public void AddImageUrlFormLocalRes(){
+
+        HashMapForLocalRes = new HashMap<String, Integer>();
+
+        HashMapForLocalRes.put("product1", R.drawable.demo3);
+        HashMapForLocalRes.put("product2", R.drawable.demo3);
+        HashMapForLocalRes.put("product3", R.drawable.demo3);
+
+
+    }
+
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+
     }
 }
