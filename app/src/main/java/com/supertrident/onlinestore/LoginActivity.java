9fc6589b661e981df870ba457cb47b640d3a686c
pratikkate3 <pratikrkate@gmail.com>
2021-02-09 19:54:02 +0530

    Login Added

---
+++ app/src/main/java/com/supertrident/onlinestore/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.supertrident.onlinestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextInputLayout emailEntered, passwordEntered;
+    Button btn_sign_in, btn_sign_up;
+    TextView btn_forget_password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+
+        btn_sign_in = findViewById(R.id.sign_in);
+        btn_forget_password = findViewById(R.id.forgot_password);
+        btn_sign_up = findViewById(R.id.sign_up_btn);
+        emailEntered = findViewById(R.id.logIn_email);
+        passwordEntered = findViewById(R.id.logIn_password);
+
+
+        btn_sign_in.setOnClickListener(v -> {
+            startActivity(new Intent(this,MainActivity.class));
+        });
+    }
+}
