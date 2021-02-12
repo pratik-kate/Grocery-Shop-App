3c1954dcd6e190af775cb848d2f3a5144dbaf85f
pratikkate3 <pratikrkate@gmail.com>
2021-02-12 20:24:35 +0530

    UI Updated

--- app/src/main/java/com/supertrident/ecom/test/ShopActivity.java
+++ app/src/main/java/com/supertrident/ecom/test/ShopActivity.java
@@ -35,7 +35,8 @@ public class ShopActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
-        getSupportActionBar().hide();
+        getSupportActionBar().setTitle("Products");
+
 
         Intent intent = getIntent();
         catId = intent.getIntExtra(MainActivity.CATID,0);
